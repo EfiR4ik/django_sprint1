6d1a5270230cd7a227bbcccde3fe9e4d7d0f3c84
EfiR4ik <justefir@gmail.com>
2024-11-13 20:03:06 +0300

    first commit

---
+++ blogicum/pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about(request):
+    template_name = 'pages/about.html'
+    return render(request, template_name)
+
+def rules(request):
+    template_name = 'pages/rules.html'
+    return render(request, template_name)
+
+def page_not_found(request, exception):
+    return render(request, "pages/404.html", status=404)
